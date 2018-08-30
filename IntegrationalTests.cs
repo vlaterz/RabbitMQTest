230782eeb46ce382b521c388cfef432b36a25293
vlaterz <33147329+vlaterz@users.noreply.github.com>
2018-08-30 15:17:14 +0300

    Add files via upload
    Do not forget to add all references needed.
    --RabbitMQ test with connections drop--
    2 tests do not pass correctly.

---
+++ IntegrationalTests.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RabbitMQ.Client;
+using Proxy;
+
+namespace IntegrationalTests
+{
+    [TestClass]
+    public class IntegrationalTests
+    {
+
+        [TestMethod]ы
+        public void DisposeConnectionTest()
+        {
+            ExecuteNetworkRepairTest(proxy =>
+                proxy.Execute(connection =>
+                    connection.Dispose()));
+        }
+
+        [TestMethod]
+        public void DropTraffic_ClientToRemote_Test()
+        {
+
+            ExecuteNetworkRepairTest(proxy =>
+                proxy.Execute(connection => connection.CopyClientToRemote.DropTraffic(true)));
+
+        }
+
+        [TestMethod]
+        public void SuspendTransmission_ClientToRemote_Test()
+        {
+
+            ExecuteNetworkRepairTest(proxy =>
+                proxy.Execute(connection => connection.CopyClientToRemote.SuspendTransmission(true)));
+
+        }
+
+        [TestMethod]
+        public void DropTraffic_RemoteToClient_Test()
+        {
+
+            ExecuteNetworkRepairTest(proxy =>
+                proxy.Execute(connection => connection.CopyRemoteToClient.DropTraffic(true)));
+        }
+
+        [TestMethod]
+        public void SuspendTransmission_RemoteToClient_Test()
+        {
+
+            ExecuteNetworkRepairTest(proxy =>
+                proxy.Execute(connection => connection.CopyRemoteToClient.SuspendTransmission(true)));
+        }
+
+
+        [TestMethod]
+        public void DropTaffic_BothConnections_Test()
+        {
+            ExecuteNetworkRepairTest(proxy => proxy.Execute(connection =>
+            {
+                connection.CopyRemoteToClient.DropTraffic(true);
+                connection.CopyClientToRemote.DropTraffic(true);
+            }));
+        }
+
+        [TestMethod]
+        public void SuspendClientToRemoteDropRemoteToClientTest()
+        {
+            ExecuteNetworkRepairTest(proxy => proxy.Execute(connection =>
+            {
+                connection.CopyRemoteToClient.DropTraffic(true);
+                connection.CopyClientToRemote.SuspendTransmission(true);
+            }));
+        }
+
+        [TestMethod]
+        public void SuspendRemoteToClientDropClientToRemoteTest()
+        {
+            ExecuteNetworkRepairTest(proxy => proxy.Execute(connection =>
+            {
+                connection.CopyRemoteToClient.SuspendTransmission(true);
+                connection.CopyClientToRemote.DropTraffic(true);
+            }));
+        }
+
+        [TestMethod]
+        public void SuspendTransmission_BothConnections_Test()
+        {
+            ExecuteNetworkRepairTest(proxy => proxy.Execute(connection =>
+            {
+                connection.CopyRemoteToClient.SuspendTransmission(true);
+                connection.CopyClientToRemote.SuspendTransmission(true);
+            }));
+        }
+
+
+        private void ExecuteNetworkRepairTest(Action<TcpDistruptingProxy> distruptionScenario)
+        {
+            Console.WriteLine(typeof(ConnectionFactory).Assembly.GetName().FullName);
+            const int targetPort = 5672;
+            const int proxyPort = 56721;
+            const string targetHost = "127.0.0.1";
+
+            var errros = 0;
+
+            var messages = new List<string>();
+            var gotMessages = new List<string>();
+            string queueName = $"mytestqueueoverproxy_{Guid.NewGuid():N}";
+
+            using (var distruptingProxy = new TcpDistruptingProxy(targetHost, targetPort, proxyPort))
+            {
+                var cf = new ConnectionFactory
+                {
+                    Uri = new Uri($"amqp://127.0.0.1:{proxyPort}/"),
+                    HostName = "127.0.0.1",
+                    Port = proxyPort,
+                    UserName = "guest",
+                    Password = "guest",
+                    AutomaticRecoveryEnabled = true,
+                    RequestedHeartbeat = 2,
+                    NetworkRecoveryInterval = TimeSpan.FromSeconds(5),
+                };
+
+                var reconnectionCount = 0;
+                var disconnectionCount = 0;
+                using (var connection = cf.CreateConnection())
+                {
+                    connection.ConnectionShutdown += (sender, args) => disconnectionCount++;
+                    connection.RecoverySucceeded += (sender, args) => reconnectionCount++;
+
+                    using (var model = connection.CreateModel())
+                    {
+                        model.QueueDeclare(queueName, false, false, false, null);
+                        model.ConfirmSelect();
+                        for (var i = 0; i < 100000; i++) //100000 messages
+                        {
+                            if (i == 10) distruptionScenario(distruptingProxy);
+
+                            var message = $"message_{Guid.NewGuid()}_{i}";
+                            try
+                            {
+                                var body = Encoding.UTF8.GetBytes(message);
+                                model.BasicPublish(exchange: "",
+                                    routingKey: queueName,
+                                    basicProperties: null,
+                                    body: body);
+                                model.WaitForConfirms();
+
+                                messages.Add(message);
+                                if (errros > 0) break;
+                            }
+                            catch (Exception)
+                            {
+                                ++errros;
+                            }
+
+                            Thread.Sleep(50);
+                        }
+
+                        Assert.AreNotEqual(0, disconnectionCount);
+                        Assert.AreNotEqual(0, reconnectionCount);
+
+                    }
+                }
+            }
+
+            var cf2 = new ConnectionFactory
+            {
+                Uri = new Uri($"amqp://{targetHost}:{targetPort}/"),
+                HostName = targetHost,
+                Port = targetPort,
+                UserName = "guest",
+                Password = "guest",
+                AutomaticRecoveryEnabled = true,
+                RequestedHeartbeat = 2,
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(5)
+            };
+            using (var cn2 = cf2.CreateConnection())
+            {
+                using (var ch2 = cn2.CreateModel())
+                {
+                    for (;;)
+                    {
+                        var basicGetResult = ch2.BasicGet(queueName, true);
+                        if (basicGetResult == null) break;
+                        gotMessages.Add(Encoding.UTF8.GetString(basicGetResult.Body));
+                    }
+
+                    ch2.QueueDelete(queueName);
+                }
+            }
+
+            Assert.AreNotEqual(0, errros);
+            Assert.IsTrue(messages.Count > 10);
+            Assert.AreEqual(messages.Count, gotMessages.Count);
+            for (var i = 0; i < messages.Count; i++)
+                Assert.AreEqual(messages[i], gotMessages[i]);
+        }
+    }
+}
---
+++ TcpDistruptingProxy.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Proxy
+{
+    public class TcpDistruptingProxy : IDisposable
+    {
+        public class Streamer
+        {
+            private readonly Socket _fromSocket;
+            private readonly Socket _toSocket;
+            private volatile bool _dropTraffic;
+            private readonly ManualResetEventSlim _suspendLatch = new ManualResetEventSlim(true);
+
+            public void DropTraffic(bool value)
+            {
+                _dropTraffic = value;
+            }
+
+            public void SuspendTransmission(bool value)
+            {
+                if (value)
+                    _suspendLatch.Reset();
+                else
+                    _suspendLatch.Set();
+            }
+
+
+            public Streamer(Socket fromSocket, Socket socket)
+            {
+                _fromSocket = fromSocket;
+                _toSocket = socket;
+            }
+
+            //Шлём в конечный сокет
+            public void DoCopy()
+            {
+                var buffer = new byte[4096];
+                try
+                {
+                    for (; ; )
+                    {
+                        _suspendLatch.Wait();
+                        var bytesRead = _fromSocket.Receive(buffer);
+                        if (bytesRead <= 0) break;
+                        _suspendLatch.Wait();
+                        if (_dropTraffic) continue;
+                        _toSocket.Send(buffer, 0, bytesRead, SocketFlags.None);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("(Streamer::DoCopy)" + e);
+
+                    //throw;
+                }
+            }
+        }
+        public class ClientConnection : IDisposable
+        {
+            private readonly TcpDistruptingProxy _tcpDistruptingProxy;
+            private readonly Socket _client;
+            private readonly Socket _remote;
+            public readonly Streamer CopyClientToRemote;
+            public readonly Streamer CopyRemoteToClient;
+
+
+            public ClientConnection(TcpDistruptingProxy tcpDistruptingProxy, Socket client)
+            {
+                _tcpDistruptingProxy = tcpDistruptingProxy;
+                _client = client;
+                _remote = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                tcpDistruptingProxy.RegisterClient(this);
+                _remote.Connect(tcpDistruptingProxy.TargetHost, tcpDistruptingProxy.TargetPort);
+                CopyClientToRemote = StartCopy(client, _remote);
+                CopyRemoteToClient = StartCopy(_remote, client);
+            }
+
+
+
+            private Streamer StartCopy(Socket fromSocket, Socket toSocket)
+            {
+                var thread = new Thread(StartCopyThread)
+                {
+                    Name = "StartCopyThread"
+                };
+                var streamer = new Streamer(fromSocket, toSocket);
+                thread.Start(streamer);
+                return streamer;
+            }
+
+            private void StartCopyThread(object obj)
+            {
+                ((Streamer)obj).DoCopy();
+                Dispose();
+            }
+
+            public void Dispose()
+            {
+                try
+                {
+                    _tcpDistruptingProxy.UnRegisterClient(this);
+                    _remote.Dispose();
+                    _client.Dispose();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("ClientConnection::Dispose" + e);
+                    // throw;
+                }
+
+            }
+        }
+
+
+        public readonly string TargetHost;
+        public readonly int TargetPort;
+        private readonly Socket _listenerSocket;
+
+        public TcpDistruptingProxy(string targetHost, int targetPort, int localPoint)
+        {
+            TargetHost = targetHost;
+            TargetPort = targetPort;
+            _listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _listenerSocket.Bind(new IPEndPoint(IPAddress.Loopback, localPoint));
+            _listenerSocket.Listen(16);
+            var acceptThread = new Thread(StartAcceptThread)
+            {
+                Name = "AcceptThread"
+            };
+            acceptThread.Start();
+        }
+
+        //Слушаем порт
+        private void StartAcceptThread()
+        {
+            while (true)
+            {
+                var client = _listenerSocket.Accept();
+                // ReSharper disable once ObjectCreationAsStatement
+                new ClientConnection(this, client);
+            }
+            // ReSharper disable once FunctionNeverReturns
+        }
+
+        public void Execute(Action<ClientConnection> action)
+        {
+            try
+            {
+                ClientConnection[] array;
+                lock (_syncRoot)
+                {
+                    array = _clientProcessors.ToArray();
+                }
+
+                foreach (var clientProcessor in array)
+                {
+                    action(clientProcessor);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("EXECUTE():"+e);
+                //throw;
+            }
+        }
+
+        private readonly List<ClientConnection> _clientProcessors = new List<ClientConnection>();
+        private readonly object _syncRoot = new object();
+
+        public void UnRegisterClient(ClientConnection clientConnection)
+        {
+            lock (_syncRoot)
+            {
+                _clientProcessors.Remove(clientConnection);
+            }
+        }
+
+        public void RegisterClient(ClientConnection clientConnection)
+        {
+            lock (_syncRoot)
+            {
+                _clientProcessors.Add(clientConnection);
+            }
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _listenerSocket.Dispose();
+                Execute(connection => connection.Dispose());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Dispose():" + e);
+                //throw;
+            }
+           
+        }
+
+       
+    }
+}
